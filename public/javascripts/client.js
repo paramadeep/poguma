46c09d6c48bde003fe10da2afab80517b39fc685
karthik <karthiksibox@gmail.com>
2014-07-06 16:34:23 +0530

    [karthik] views with polymer

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,15 +1,13 @@
 console.log("vandiruchi");
 
 $.get("/cards",function(data){
+
   var cards=d3.select("#chart")
     .selectAll("div")
     .data(data);
 
-  var cards_entered=cards.enter().append("div").attr('class','card');
-  cards_entered.append("div").attr('class','dev')
-    .style("width", function(d) { return d.dev_pending + "px"; })
-    .text(function(d) { return d.dev_pending;});
-  cards_entered.append("div").attr('class','qa')
-    .style("width", function(d) { return d.qa_pending + "px"; })
-    .text(function(d) { return d.qa_pending;})
+  var cards_entered=cards.enter().append("div").attr('class','card').append("div").text(function(d){return d.id});
+  cards_entered.append("paper-progress").attr('value',function(d){return d.dev_pending*10});
+
+  cards_entered.append("paper-progress").attr('value',function(d){return d.qa_pending*10})
 })
