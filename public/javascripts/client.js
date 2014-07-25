410a66fbcd811113054a813244810801bb29e2d8
karthik <karthiksibox@gmail.com>
2014-07-25 16:43:48 +0530

    [Deepak/Karthik] Moved estimates computation to server

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,13 +1,19 @@
 console.log("vandiruchi");
 
+function render_page(){
 $.get("/card_details",function(data){
-
+  //data=clean_up_data(data);
+console.log("inside");
   var cards=d3.select("#chart")
     .selectAll("div")
-    .data(data);
+    .data(data)
+  var cards_entered=cards.enter().append("div").attr('class','card').text(function(d){return d.number});
+  
+  cards_entered.append("paper-progress").attr('value',function(d){return d.dev_effort*10});
 
-  var cards_entered=cards.enter().append("div").attr('class','card').append("div").text(function(d){return d.id});
-  cards_entered.append("paper-progress").attr('value',function(d){return d.dev_pending*10});
+  cards_entered.append("paper-progress").attr('value',function(d){return d.qa_effort*10});
+});
+};
 
-  cards_entered.append("paper-progress").attr('value',function(d){return d.qa_pending*10})
-})
+
+$(render_page);
--- routes/index.js
+++ routes/index.js
@@ -9,16 +9,44 @@ exports.cards =function(req, res){
 
   var response;
   var mingle_query="select 'name','Story Status','number','Estimate','QA Estimate' where 'Story Status' >= 'Ready for Development' and 'Story Status' < 'Ready for Signoff' and 'Planned Release' = (Current Release)";
-
+  res.set({
+    'Content-Type': 'application/json',
+  });
   var request = require('request');
   var mingle = require('../mingle.json');
   var encoded_mql=encodeURIComponent(mingle_query);
   var url = "https://" + mingle.user_name + ":" + mingle.password + "@" + mingle.domain + "/api/v2/projects/" + mingle.project_name + "/cards/execute_mql.json?mql=" + encoded_mql;
   request(url,function(er,re,body){
+    body=clean_up_data(JSON.parse(body));
     res.send(body);
   });
 };
 
+
+function clean_up_data(cards){
+  var clean_data=cards.map(function(card){
+    return {
+      'name': card.Name, 
+      'number': card.Number,
+      'qa_effort': get_qa_effort(card),
+      'dev_effort': get_dev_effort(card)
+    };
+  });
+  debugger;
+
+  return clean_data;
+};
+
+
+function get_qa_effort(card){
+  var qa_scale= {'Simple': 1, 'Medium': 2, 'Complex': 3};
+  return card['QA Estimate']!= null ? qa_scale[card['QA Estimate']] : 0;
+
+}
+
+function get_dev_effort(card){
+ return card['Estimate']!= null ? parseInt(card['Estimate']) : 0;
+}
 
 
 
