245506946d851cde6d8c8f3850fe783866413114
karthik <karthiksibox@gmail.com>
2014-07-27 15:32:59 +0530

    [Karthik] Including timeline view

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,19 +1,44 @@
 console.log("vandiruchi");
 
-function render_page(){
-$.get("/card_details",function(data){
-  //data=clean_up_data(data);
-console.log("inside");
-  var cards=d3.select("#chart")
-    .selectAll("div")
-    .data(data)
-  var cards_entered=cards.enter().append("div").attr('class','card').text(function(d){return d.number});
-  
-  cards_entered.append("paper-progress").attr('value',function(d){return d.dev_effort*10});
+// Timeline Options
+var options = {
+  'width':  '100%',
+  'editable': true, // make the events dragable
+  'style': 'box',
+  'intervalMax': 1000 * 60 * 60 * 24 * 90,
+  'start': "07-01-2014",
+  'zoomMin': 86400000,
+  'zoomable': false,
+  'scale': links.Timeline.StepDate.SCALE.MONTH,
+};
+var timeline = new links.Timeline(document.getElementById('chart'),options);
 
-  cards_entered.append("paper-progress").attr('value',function(d){return d.qa_effort*10});
-});
+
+function render_page(){
+  $.get("/card_details",function(cards){
+    console.log("inside");
+    var data = [];
+
+    cards.forEach(function(card){
+      var date=new Date();
+      card['className']='';
+      if(card.dev_effort > 0){
+        date.setDate(date.getDate()+card.dev_effort);
+        card['className']= 'dev_in_progress';
+      }
+      if(card.qa_effort > 0){
+        date.setDate(date.getDate()+card.qa_effort);
+        card['className']= card['className']+' qa_in_progress';
+      }
+      card['start']=  Date.parse(date);
+      card['content']=card.number;
+      timeline.addItem(card);
+
+    });
+  });
 };
 
 
 $(render_page);
+
+
